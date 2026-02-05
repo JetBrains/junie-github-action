f725664b08338f1bfb2e3fd0670b72a0ca7d5427
Marco Damonte <marcodamonte@jetbrains.com>
2026-02-05 15:05:30 +0000

    test

--- src/github/validation/trigger.ts
+++ src/github/validation/trigger.ts
@@ -77,6 +77,7 @@ export function detectJunieTriggerPhrase(context: JunieExecutionContext): boolea
             return true;
         }
     }
+
     const hasTrigger = isReviewOrCommentHasTrigger(context, triggerPhraseRegex)
 
     if (hasTrigger) {
