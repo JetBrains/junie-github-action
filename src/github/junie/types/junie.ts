77bd7a80ee12cd2bb12d48a913eb6c7337fa72b1
Nikita Barkov <nikita.barkov@jetbrains.com>
2026-02-12 11:45:32 +0100

    Revert "Revert codeReview task type"
    This reverts commit 2add53164d14f5e53172a7a8b573f028015dae36.

--- src/github/junie/types/junie.ts
+++ src/github/junie/types/junie.ts
@@ -13,7 +13,13 @@ export interface MergeTask {
     branch: string;
 }
 
+export interface CodeReview {
+    description?: string;
+    diffCommand?: string;
+}
+
 export interface CliInput {
     task?: string;
     mergeTask?: MergeTask;
+    codeReview?: CodeReview;
 }
