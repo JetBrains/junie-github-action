2f4b3ca7e8a73af42f7fc84e225ae1ec9c7155b8
Mariia.Fadeeva <mariia.fadeeva@jetbrains.com>
2026-03-25 12:52:38 +0200

    Passed `YOUTRACK_TOKEN` from secrets in workflows.

--- test/config/test-config.ts
+++ test/config/test-config.ts
@@ -9,7 +9,7 @@ interface E2EConfig {
 export const e2eConfig: E2EConfig = {
     githubToken: process.env.GITHUB_TOKEN || "",
     org: process.env.TEST_ORG || "melotria",
-    youtrackToken: process.env.YOUTRACK_TOKEN || "perm-TWFyaWlhLkZhZGVldmE=.NDItNA==.iabFGqqwIwtTldJUi2dvFm3fkxWEGW",
+    youtrackToken: process.env.YOUTRACK_TOKEN || "",
     youtrackBaseUrl: process.env.YOUTRACK_BASE_URL || "https://nikitajunietest.youtrack.cloud",
     youtrackProjectId: process.env.YOUTRACK_PROJECT_ID || "TP",
 };
